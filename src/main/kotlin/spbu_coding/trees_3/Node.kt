45f07cfaef92e953f262c8951bde480cfaf645dc
IlyaMuravjov <muravjovilya@gmail.com>
2021-04-01 00:36:52 +0300

    Implement Node methods, make `left`, `right` and `parent` constructor arguments optional

--- src/main/kotlin/spbu_coding/trees_3/Node.kt
+++ src/main/kotlin/spbu_coding/trees_3/Node.kt
@@ -6,14 +6,20 @@ class Node<K, V, T>(
     override val key: K,
     override var value: V,
     var balancerData: T,
-    var parent: NodeParent<K, V, T>,
-    var left: Node<K, V, T>?,
-    var right: Node<K, V, T>?
+    var left: Node<K, V, T>? = null,
+    var right: Node<K, V, T>? = null,
+    parent: NodeParent<K, V, T>? = null
 ) : NodeParent<K, V, T>(), MutableMap.MutableEntry<K, V> {
-    override fun setValue(newValue: V): V = TODO()
-    override fun toString(): String = TODO()
-    override fun hashCode(): Int = TODO()
-    override fun equals(other: Any?): Boolean = TODO()
+    lateinit var parent: NodeParent<K, V, T>
+
+    init {
+        if (parent != null) this.parent = parent
+    }
+
+    override fun setValue(newValue: V): V = value.also { value = newValue }
+    override fun toString() = "$key=$value"
+    override fun hashCode(): Int = (key?.hashCode() ?: 0) xor (value?.hashCode() ?: 0)
+    override fun equals(other: Any?): Boolean = other is Map.Entry<*, *> && key == other.key && value == other.value
 }
 
 data class RootHolder<K, V, T>(var root: Node<K, V, T>? = null) : NodeParent<K, V, T>()
