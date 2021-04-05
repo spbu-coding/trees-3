c484d864151c695d33d6f71494a78824e38e5a4d
IlyaMuravjov <muravjovilya@gmail.com>
2021-04-05 20:56:02 +0300

    Finish implementing BinarySearchTree, add BinarySearchTree tests

--- src/main/kotlin/spbu_coding/trees_3/BinarySearchTree.kt
+++ src/main/kotlin/spbu_coding/trees_3/BinarySearchTree.kt
@@ -4,126 +4,96 @@ import spbu_coding.trees_3.NodeSide.LEFT
 import spbu_coding.trees_3.NodeSide.RIGHT
 import kotlin.collections.MutableMap.MutableEntry
 
-class BinarySearchTree<K, V, T>(private val balancer: TreeBalancer<T>, private val comparator: Comparator<K>) :
-    AbstractMutableMap<K, V>() {
-
+class BinarySearchTree<K, V, T>(
+    private val balancer: TreeBalancer<T>,
+    private val comparator: Comparator<K>
+) : AbstractMutableMap<K, V>() {
     override var size: Int = 0
-    override val entries: MutableSet<MutableEntry<K, V>> = EntrySet()
-    var rootHolder = RootHolder<K, V, T>()
-    var modCount = 0
-
-    override fun containsKey(key: K): Boolean {
-        return getNode(key) != null
-    }
-
-    override fun get(key: K): V? {
-        return getNode(key)?.value
-    }
-
-    override fun clear() {
-        if (rootHolder.root != null) {
-            rootHolder.root = null
+        private set(value) {
+            field = value
             modCount++
-            size = 0
         }
+    override val entries: MutableSet<MutableEntry<K, V>> = EntrySet()
+    private var rootHolder = RootHolder<K, V, T>()
+    private var modCount = 0
+
+    override fun containsKey(key: K): Boolean = getNode(key) != null
+    override fun get(key: K): V? = getNode(key)?.value
+    override fun clear() {
+        rootHolder.root = null
+        size = 0
     }
 
     override fun remove(key: K): V? {
-        val node = getNode(key)
-        if (node == null) return null
+        val node = getNode(key) ?: return null
         removeNode(node)
         return node.value
     }
 
     override fun put(key: K, value: V): V? {
-        if (rootHolder.root == null) {
-            val node = Node(key, value, balancer.initialNodeBalancerData)
-            rootHolder.attachRoot(node)
-            balancer.rebalanceAfterInsertion(node)
-            modCount++
-            size++
-            return null
-        }
-        var cur = rootHolder.root!!
-        while (true) {
-            if (comparator.compare(key, cur.key) < 0) {
-                if (cur.left == null) {
-                    val node = Node(key, value, balancer.initialNodeBalancerData)
-                    cur.attachChild(LEFT, node)
-                    balancer.rebalanceAfterInsertion(node)
-                    modCount++
-                    size++
-                    return null
-                }
-                cur = cur.left!!
-            } else if (comparator.compare(key, cur.key) > 0) {
-                if (cur.right == null) {
-                    val node = Node(key, value, balancer.initialNodeBalancerData)
-                    cur.attachChild(RIGHT, node)
-                    balancer.rebalanceAfterInsertion(node)
-                    modCount++
-                    size++
-                    return null
-                }
-                cur = cur.right!!
-            } else {
-                val oldValue = cur.value
-                cur.value = value
-                return oldValue
+        var cur = rootHolder.root ?: return null.also { insertLeaf(key, value) { rootHolder.attachRoot(it) } }
+        while (true)
+            when (val side = NodeSide.fromSignOf(comparator.compare(key, cur.key))) {
+                null -> return cur.setValue(value)
+                else -> cur = cur[side] ?: return null.also { insertLeaf(key, value) { cur.attachChild(side, it) } }
             }
-        }
     }
 
-    fun getNode(key: K): Node<K, V, T>? {
-        if (rootHolder.root == null) return null
-        var cur = rootHolder.root!!
-        while (true) {
-            if (comparator.compare(key, cur.key) < 0) {
-                if (cur.left == null) return null
-                cur = cur.left!!
-            } else if (comparator.compare(key, cur.key) > 0) {
-                if (cur.right == null) return null
-                cur = cur.right!!
-            } else return cur
-        }
+    private fun getNode(key: K): Node<K, V, T>? {
+        var cur = rootHolder.root ?: return null
+        while (true)
+            when (val side = NodeSide.fromSignOf(comparator.compare(key, cur.key))) {
+                null -> return cur
+                else -> cur = cur[side] ?: return null
+            }
     }
 
-    fun removeNode(node: Node<K, V, T>) {
+    private fun removeNode(node: Node<K, V, T>) {
         ifParentOfTwoSwapNonUserDataWithSuccessor(node)
         node.deleteNodeWithAtMostOneChild()
         balancer.rebalanceAfterDeletion(node)
-        modCount++
         size--
     }
 
+    private inline fun insertLeaf(key: K, value: V, leafInit: (Node<K, V, T>) -> Unit) {
+        balancer.rebalanceAfterInsertion(Node(key, value, balancer.initialNodeBalancerData).also(leafInit))
+        size++
+    }
+
     private inner class EntrySet : AbstractMutableSet<MutableEntry<K, V>>() {
-        override val size: Int = this@BinarySearchTree.size
+        override val size: Int get() = this@BinarySearchTree.size
 
-        override fun add(element: MutableEntry<K, V>): Boolean {
-            throw UnsupportedOperationException()
-        }
-
-        override fun clear() {
-            this@BinarySearchTree.clear()
-        }
-
-        override fun iterator(): MutableIterator<MutableEntry<K, V>> {
-            return EntryIterator()
-        }
-
-        override fun remove(element: MutableEntry<K, V>): Boolean {
-            return this@BinarySearchTree.remove(element.key, element.value)
-        }
-
-        override fun contains(element: MutableEntry<K, V>): Boolean {
-            return getNode(element.key) == element
-        }
+        override fun add(element: MutableEntry<K, V>) = throw UnsupportedOperationException()
+        override fun clear() = this@BinarySearchTree.clear()
+        override fun iterator(): MutableIterator<MutableEntry<K, V>> = EntryIterator()
+        override fun remove(element: MutableEntry<K, V>) = this@BinarySearchTree.remove(element.key, element.value)
+        override fun contains(element: MutableEntry<K, V>) = getNode(element.key) == element
     }
 
     private inner class EntryIterator : MutableIterator<MutableEntry<K, V>> {
-        override fun hasNext() = TODO()
-        override fun next(): MutableEntry<K, V> = TODO()
-        override fun remove() = TODO()
+        private var lastReturned: Node<K, V, T>? = null
+        private var next: Node<K, V, T>? = rootHolder.root?.farthestDescendent(LEFT)
+        private var expectedModCount = modCount
+
+        override fun hasNext(): Boolean {
+            if (modCount != expectedModCount) throw ConcurrentModificationException()
+            return next != null
+        }
+
+        override fun next(): MutableEntry<K, V> {
+            if (modCount != expectedModCount) throw ConcurrentModificationException()
+            if (next == null) throw NoSuchElementException()
+            lastReturned = next
+            next = next!!.nextToTheSide(RIGHT)
+            return lastReturned!!
+        }
+
+        override fun remove() {
+            if (modCount != expectedModCount) throw ConcurrentModificationException()
+            removeNode(checkNotNull(lastReturned))
+            expectedModCount = modCount
+            lastReturned = null
+        }
     }
 
     companion object {
@@ -134,8 +104,24 @@ class BinarySearchTree<K, V, T>(private val balancer: TreeBalancer<T>, private v
         fun <K : Comparable<K>, V> of(balancer: TreeBalancer<*>): BinarySearchTree<K, V, *> =
             BinarySearchTree(balancer, naturalOrder())
 
-        fun <K, V, T> ifParentOfTwoSwapNonUserDataWithSuccessor(node: Node<K, V, T>) {
-            TODO()
+        // internal for testing
+        internal fun <K, V, T> ifParentOfTwoSwapNonUserDataWithSuccessor(node: Node<K, V, T>) {
+            if (node.has(LEFT) && node.has(RIGHT)) {
+                val succ = node.right!!.farthestDescendent(LEFT)
+                node.balancerData = succ.balancerData.also { succ.balancerData = node.balancerData }
+                succ.attachChild(LEFT, node.left)
+                node.left = null // `succ` used to be leftest so it didn't have left child
+                if (succ === node.right) {
+                    succ.attachInPlaceOf(node)
+                    node.attachChild(RIGHT, succ.right)
+                    succ.attachChild(RIGHT, node)
+                } else { // `succ` is left child of some descendant
+                    val oldParent = node.parent
+                    node.attachInPlaceOf(succ)
+                    oldParent.replaceChild(node, succ)
+                    succ.attachChild(RIGHT, node.right.also { node.attachChild(RIGHT, succ.right) })
+                }
+            }
         }
     }
 }
