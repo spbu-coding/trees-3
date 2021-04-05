fc1067b961187e8c2d9288202cba8189cf76516d
lenya <66315146+Lev0nid@users.noreply.github.com>
2021-04-05 13:24:59 +0300

    Add RedBlackTreeBalancer

--- src/main/kotlin/spbu_coding/trees_3/balancers/RedBlackTreeBalancer.kt
+++ src/main/kotlin/spbu_coding/trees_3/balancers/RedBlackTreeBalancer.kt
@@ -1,7 +1,8 @@
 package spbu_coding.trees_3.balancers
 
-import spbu_coding.trees_3.Node
-import spbu_coding.trees_3.TreeBalancer
+import spbu_coding.trees_3.*
+import spbu_coding.trees_3.NodeSide.*
+import spbu_coding.trees_3.balancers.NodeColor.*
 
 enum class NodeColor {
     RED, BLACK
@@ -10,7 +11,86 @@ enum class NodeColor {
 typealias RBNode<K, V> = Node<K, V, NodeColor>
 
 object RedBlackTreeBalancer : TreeBalancer<NodeColor> {
-    override val initialNodeBalancerData: NodeColor get() = TODO()
-    override fun <K, V> rebalanceAfterInsertion(node: RBNode<K, V>) = TODO()
-    override fun <K, V> rebalanceAfterDeletion(node: RBNode<K, V>) = TODO()
+    var RBNode<*, *>?.color: NodeColor
+        get() = this?.balancerData ?: BLACK
+        set(value) {
+            if (this != null) {
+                balancerData = value
+            }
+        }
+
+    private fun <K, V> RBNode<K, V>.sibling() = parentNode?.let { it[side!!.opposite] }
+
+    private fun <K, V> RBNode<K, V>.uncle() = parentNode?.sibling()
+
+    private fun <K, V> RBNode<K, V>.grandparent() = parentNode?.parentNode
+
+    private fun <K, V> rotate(side: NodeSide, node: RBNode<K, V>) {
+        val c = node[side.opposite]!!
+        val gc = c[side]
+        c.attachInPlaceOf(node)
+        node.attachChild(side.opposite, gc)
+        c.attachChild(side, node)
+    }
+
+    override val initialNodeBalancerData: NodeColor get() = RED
+
+    override fun <K, V> rebalanceAfterInsertion(node: RBNode<K, V>) {
+        var node = node
+        while (!node.isRoot && node.parentNode?.color == RED) {
+            val uncle = node.uncle()
+            if (uncle.color == RED) {
+                node.parentNode?.color = BLACK
+                uncle?.color = BLACK
+                node.grandparent()?.color = RED
+                node = node.grandparent() ?: break
+            } else {
+                val side = node.parentNode?.side
+                if (node.side != node.parentNode?.side) {
+                    node = node.parentNode ?: break
+                    rotate(side!!, node)
+                }
+                node.parentNode?.color = BLACK
+                node.grandparent()?.color = RED
+                rotate(side?.opposite!!, node.grandparent()!!)
+            }
+        }
+        while (!node.isRoot) node = node.parentNode!!
+        node.color = BLACK
+    }
+
+    override fun <K, V> rebalanceAfterDeletion(node: RBNode<K, V>) {
+        if (node.color == RED)
+            return
+        var node = node.left ?: node.right ?: node
+        while (!node.isRoot && node.color == BLACK) {
+            val nodeSide = node.side ?: if (node.parentNode?.has(LEFT) == true) RIGHT else LEFT
+            val parent = node.parentNode ?: break
+            var sib = parent.get(nodeSide.opposite)
+            if (sib.color == RED) {
+                sib?.color = BLACK
+                parent.color = RED
+                rotate(nodeSide, node.parentNode ?: break)
+                sib = parent[nodeSide.opposite]
+            }
+            if (sib?.left.color == BLACK && sib?.right.color == BLACK) {
+                sib?.color = RED
+                node = parent
+            } else {
+                if (sib!!.get(nodeSide.opposite).color == BLACK) {
+                    sib.get(nodeSide)?.color = BLACK
+                    sib.color = RED
+                    rotate(nodeSide.opposite, sib)
+                    sib = parent.get(nodeSide.opposite) ?: break
+                }
+                sib.color = node.parentNode.color
+                parent.color = BLACK
+                sib.get(nodeSide.opposite)?.color = BLACK
+                rotate(nodeSide, parent)
+                while (!node.isRoot) node = node.parentNode!!
+                break
+            }
+        }
+        node.color = BLACK
+    }
 }
